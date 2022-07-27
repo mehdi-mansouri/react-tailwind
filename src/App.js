f467f474910dc033b18d6909ece9b8c4f589b48f
Mehdi mansouri <mhd67g@gmail.com>
2022-07-27 19:07:51 +0200

    view

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      React UND TailwindCss
+    <div className='content-center'>
+      <h1 className="bg-slate-800 animate-bounce shadow-2xl text-amber-50 text-center w-48 m-10 rounded">
+        Hello world!
+      </h1>
+      <button className='btn'>Drücken</button>
+     
     </div>
+
   );
 }
 
